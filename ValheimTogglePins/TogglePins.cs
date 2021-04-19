b276420b614baf344fb80e24a0e6c5e06c596723
Steven Royer <seroyer@hotmail.com>
2021-04-18 20:35:31 -0500

    Initial harmony patch version
    Signed-off-by: Steven Royer <seroyer@hotmail.com>

---
+++ ValheimTogglePins/TogglePins.cs
@@ -0,0 +1,59 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ValheimTogglePins
+{
+    [BepInPlugin("seroyer.ValheimTogglePins", "Valheim Toggle Pins", "1.0.0")]
+    [BepInProcess("valheim.exe")]
+    public class TogglePins : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("seroyer.ValheimTogglePins");
+
+        private static bool m_keyInit = false;
+        private static bool m_pinsVisible = true;
+
+        void Awake()
+        {
+            harmony.PatchAll();
+        }
+
+        void OnDestroy()
+        {
+            harmony.UnpatchSelf();
+        }
+
+        [HarmonyPatch(typeof(ZInput), "Reset")]
+        public static class ZInput_Reset_Patch
+        {
+            public static void Postfix(ref ZInput __instance)
+            {
+                __instance.AddButton("TogglePins", KeyCode.T);
+                m_keyInit = true;
+            }
+        }
+
+        [HarmonyPatch(typeof(Minimap), "IsPointVisible")]
+        public static class Minimap_IsPointVisible_Patch
+        {
+            public static bool Prefix(ref bool __result, Vector3 p, RawImage map)
+            {
+                if (!m_pinsVisible)
+                {
+                    __result = false;
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        void Update()
+        {
+            if (m_keyInit && ZInput.GetButtonDown("TogglePins"))
+            {
+                m_pinsVisible = !m_pinsVisible;
+            }
+        }
+    }
+}
